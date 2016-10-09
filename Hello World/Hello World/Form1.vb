c1ed01706dc4b7e29dc9c0cf3e625652424b9f3e
Richal Vestert <fang-nie@web.de>
2016-10-09 20:21:39 +0200

    Erster Commit

---
+++ Hello World/Hello World/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "Hallo Welt"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = "2. Hallo Welt"
+    End Sub
+End Class
